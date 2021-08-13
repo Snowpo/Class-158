bf833fb3dbca4e0589679f0c7b5e871791afff19
Snowpo <77101984+Snowpo@users.noreply.github.com>
2021-08-13 18:55:08 +0530

    Add files via upload

---
+++ js/CursorEvents.js
@@ -0,0 +1,92 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+  init: function () {
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvents();
+    this.handleClickEvents();
+  },
+
+  handleClickEvents: function() {
+    console.log('before click')
+    this.el.addEventListener('click',e=>{
+      const placesContainer = document.querySelector('#places-container')
+      console.log('after click')
+      const {state} = placesContainer.getAttribute('tour')
+      if(state==='places-list'){
+        const id=this.el.getAttribute('id')
+        const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+        if(placesId.includes(id)){
+          placesContainer.setAttribute('tour',{state:'view',selectedCard:id})
+        }
+      }
+      if(state==='view' || state==='change-view'){
+        this.handleViewState()
+      }
+    })
+  },
+
+  handlePlacesListState: function () {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectedItemId: id,
+      });
+      this.el.setAttribute("material", {
+        color: "#D76B30",
+        opacity: 1,
+      });
+      this.el.setAttribute("geometry",{
+        primitive:"ring",
+        radiusInner:9,
+        radiusOuter:14
+      })
+    }
+  },
+  handleMouseEnterEvents: function () {
+    // Mouse Enter Events
+    this.el.addEventListener("mouseenter", () => {
+      this.handlePlacesListState();
+    });
+  },
+  handleMouseLeaveEvents: function () { 
+    // Mouse Leave Events
+    this.el.addEventListener("mouseleave", ()=> {
+      const {selectedItemId} = this.data
+      if(selectedItemId){
+        const el=document.querySelector(`#${selectedItemId}`)
+        const id=el.getAttribute('id')
+        if(id==selectedItemId){
+          el.setAttribute('material',{
+            color:'#0077cc',
+            opacity:1
+          })
+          el.setAttribute("geometry",{  
+            primitive:"ring",
+            radiusInner:9,
+            radiusOuter:10
+          })
+        }
+      } 
+    })
+  },
+  handleViewState: function(){
+    const el = this.el
+    const id = el.getAttribute('id')
+    const placesContainer=document.querySelector('#places-container')
+    const {selectedItemId}=placesContainer.getAttribute('cursor-listener')
+    const sideviewplacesid=['place-1','place-2','place-3','place-4']
+    if(sideviewplacesid.includes(id)){
+      placesContainer.setAttribute('tour',{state:'change-view'})
+      const skyEl=document.querySelector('#main-container')
+      skyEl.setAttribute('material',{
+        src:`./assets/360_images/${selectedItemId}/${id}.jpg`,
+        color:'white'
+      })
+    }
+  }
+});
+
